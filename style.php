2ec36832f4e659072c3e46433720ba6a82b50987
MiguelCalderonPuertas <55751968+MiguelCalderonPuertas@users.noreply.github.com>
2019-11-29 17:27:36 +0100

    Add files via upload

---
+++ style.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+	<head>
+	
+		<title> Don Quijote </title>
+	
+	<style>
+
+		body  {
+			background-color: powderblue;
+		}
+		h1 {
+			color: black;
+			font-style: italic;
+			font-weight: bold;
+			text-decoration: underline;
+			text-align: center;
+		}
+	
+		h2 {
+ 			text-align: center;
+ 			margin: auto;
+			font-weight: bold;
+			font-style: italic;
+		}
+
+		h3 {
+ 			text-align: center;
+			font-weight: bold;
+			font-style: italic;
+		}
+ 		h4{
+ 			text-align: center;
+ 		}
+
+ 		h5	{
+ 			font-weight: bold;
+ 		}
+
+ 		h6 {
+ 			margin:0px;
+ 			text-align: center;
+ 			display: block;
+ 			font-size: 60px;
+			color: black;
+			font-style: italic;
+			font-weight: bold;
+			text-decoration: underline;	
+
+ 		}
+	</style>	
+
+	</head> 
+		
+		<h1> Don Quijote </h1></br>
+		<h2>En un lugar de <a href="https://es.wikipedia.org/wiki/La_Mancha">la mancha</a>  de cuyo nombre no quiero acordarme.</h2>
+		<h3><img src="https://www.aboutespanol.com/thmb/j4AajFJTJgOKagPfXt2c2uB-f6c=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Molinodevientodetrigo-0bd3a2da1d6243a385f163a2685c3cd0.jpg" alt="Smiley face" width="700" height="450">
+		</h3>
+		<h4>Un molino giraba y giraba, poco mas y volaba.</h4>
+		
+
+
+
+
+
+<table border>
+ 
+ <tr>
+    <td>Autonomía</td><td>Provincia</td>
+  </tr>
+	<tr><td>Castilla la Mancha</td><td>Albacete</td></tr>	
+	<tr><td>Castilla la Mancha</td><td>Ciudad Real</td></tr>	
+	<tr><td>Castilla la Mancha</td><td>Cuenca</td></tr>	
+	<tr><td>Castilla la Mancha</td><td>Guadalajara</td></tr>	
+	<tr><td>Castilla la Mancha</td><td>Toledo</td></tr>	
+  </table>
+
+	<h6>Nombre</h6>
+
+<h5>
+<center>
+<form>
+  First name:<br>
+  <input>
+  <br>
+  Last name:<br>
+  <input>
+  <br><br>
+  <input type="submit" value="Enviar">
+</center>
+</form> 
+</h5>
+	
+	</body>
+</html>
